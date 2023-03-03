d5b004f642e210be33a2242025adcaea683cd72b
John B. Lanier <johnblanier@gmail.com>
2023-03-03 13:47:10 -0800

    Update barrage_human_inits.py

--- stratego_env/game/inits/barrage_human_inits.py
+++ stratego_env/game/inits/barrage_human_inits.py
@@ -1,3 +1,5 @@
+# Collected from https://www.gravon.de/gravon/stratego/stratego.jsp
+
 BARRAGE_INITS = [
     "AAAAALAAKAAAAAAEAACADAAAAAAAABDAAAAAAAAM",
     "AAAAEKADDAAAAAACMLAAAAAAAAABAAAAAAAAAAAA",
